7c59e47de7659cc03fb7c6122defe7b1804d5840
Tobias Lengsholz <tobias.lengsholz@invision.de>
2019-07-31 13:57:24 +0200

    Copy definition file to dist folder on build

--- package-scripts.js
+++ package-scripts.js
@@ -56,6 +56,7 @@ module.exports = {
       script: 'doctoc README.md'
     },
     copyTypes: npsUtils.copy('src/*.js.flow dist'),
+    copyDefs: npsUtils.copy('src/*.d.ts dist'),
     lint: {
       description: 'lint the entire project',
       script: 'eslint .'
